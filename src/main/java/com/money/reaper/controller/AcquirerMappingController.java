3de6f8e3a73af100bc09451530131e2ec118a16c
jack-4711 <jack.sparow4711@gmail.com>
2024-10-11 10:12:39 +0530

    space added for commit testing

--- src/main/java/com/money/reaper/controller/AcquirerMappingController.java
+++ src/main/java/com/money/reaper/controller/AcquirerMappingController.java
@@ -10,7 +10,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController; 
 
 import com.money.reaper.dto.AcquirerMappingRequest;
 import com.money.reaper.dto.AcquirerRoutingDetailsRequest;
